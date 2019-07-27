edf8cf99905aca3cf227e7956410b571b972537e
Radhakrishnan Kuppusamy <rkuppusamy@hotmail.com>
2019-07-27 17:25:42 -0500

    delegate example

--- Example/Program.cs
+++ Example/Program.cs
@@ -8,8 +8,30 @@ namespace Example
 {
     class Program
     {
-        static void Main(string[] args)
+
+        //delegate int delPointer(int i);
+        //static int Square(int x)
+        //{
+        //    return x * x;
+        //}
+
+        //static delPointer obj = Square; //Delegate pointing to Square method..
+        //public static void Main()
+        //{
+        //    //Invoking deleaget
+        //    int Result = obj(5);
+        //    Console.WriteLine("Square :" + Result);
+        //}
+        delegate int del(int i);
+        static void Main()
         {
+            // Invoking delegate
+            del myDelegate = x => x * x;
+            int j = myDelegate(5);
+            Console.WriteLine("Square:"+ j);
+
+            Console.ReadKey();
+
         }
     }
 }
